dcdf730cb0abf5f44d5a06611741bb12e301f607
Xylar Asay-Davis <xylarstorm@gmail.com>
2024-04-11 20:34:56 +0200

    fix boz erros in mksurfdata_map build

--- components/elm/tools/mksurfdata_map/src/nanMod.F90
+++ components/elm/tools/mksurfdata_map/src/nanMod.F90
@@ -21,14 +21,14 @@ module nanMod
   save
 #ifdef __PGI
 ! quiet nan for portland group compilers
-  real(r8), parameter :: inf = O'0777600000000000000000'
-  real(r8), parameter :: nan = O'0777700000000000000000'
-  integer,  parameter :: bigint = O'17777777777'
+  real(r8), parameter :: inf = real(O'0777600000000000000000')
+  real(r8), parameter :: nan = real(O'0777700000000000000000')
+  integer,  parameter :: bigint = int(O'17777777777')
 #else
 ! signaling nan otherwise
-  real(r8), parameter :: inf = O'0777600000000000000000'
-  real(r8), parameter :: nan = O'0777610000000000000000'
-  integer,  parameter :: bigint = O'17777777777'
+  real(r8), parameter :: inf = real(O'0777600000000000000000')
+  real(r8), parameter :: nan = real(O'0777610000000000000000')
+  integer,  parameter :: bigint = int(O'17777777777')
 #endif
 !
 ! !REVISION HISTORY:
